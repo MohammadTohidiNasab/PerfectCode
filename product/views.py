4d0693ea862f7a3d42764728bb21eeb34a4070d1
mohammad <mohammadtohidi.mtn@gmail.com>
2023-01-24 15:56:40 +0330

    add comment view

--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from .serializers import ProductSerializer
-from . models import Product
+from . models import Product, Comment
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all.order_by('date')
     serializer_class = ProductSerializer
