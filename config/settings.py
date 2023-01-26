7aafcda3b10323fec5b1901c97a5d390f887dae3
mohammad <mohammadtohidi.mtn@gmail.com>
2023-01-26 16:17:24 +0330

    pagination

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
+
+
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 8
+}
