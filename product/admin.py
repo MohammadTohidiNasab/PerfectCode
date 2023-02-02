45435c7295d4e951165f2472b2946d0d70cdcf22
mohammad <mohammadtohidi.mtn@gmail.com>
2023-02-03 01:17:43 +0330

    panel amin

--- product/admin.py
+++ product/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import Product 
 # Register your models here.
 
-admin.site.register(Product)
+
+admin.site.site_header = 'پنل مدیریت سایت'
+admin.site.site_title ='مدیریت'
+admin.site.index_title = 'درگاه مدیریت سایت فروشگاه'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    ordering = ('created_at',)
+    filter_horizontal = ()
+
+    
+    
+admin.site.register(Product,ProductAdmin)
