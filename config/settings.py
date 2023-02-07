ff8a87573aeebfcb3eda50d747e9b2d14e9257a1
Mohammadreza-Rokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-02-07 21:06:37 +0330

    account login register

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
 
 
-
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
+                           'account.authentication.EmailAuthBackend']
 
 
 REST_FRAMEWORK = {
