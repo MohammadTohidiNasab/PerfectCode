b199a54998223c56fff30ec9176cd5eae039e4cb
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-02-09 03:42:15 -0800

    order url

--- order/serializers.py
+++ order/serializers.py
@@ -6,4 +6,5 @@ from .models import Ordering
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Ordering
-        fields = ('total_products', 'transaction_id')
+        fields = ('user',)
+        
--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'', views.OrderViewSet)
 
 urlpatterns = [
-
+    path('add/<str:id>/<str:token>/', views.add, name='order.add'),
+    path('', include(router.urls))
 ]
