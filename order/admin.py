bdcda16429c2dfbf1efcfe368d39f3f80b397e67
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-02-10 03:15:26 -0800

    panel admin

--- order/admin.py
+++ order/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Ordering
 # Register your models here.
 
-
-admin.site.register(Ordering)
+class OrderingAdmin(admin.ModelAdmin):
+    fields = ('user',('product_names','total_products'),'created_at','updated_at')
+    readonly_fields = ('updated_at','created_at')
+    list_display = ('user','transaction_id','created_at','updated_at','product_names','total_products')
+    save_on_top = True
+admin.site.register(Ordering,OrderingAdmin)
--- order/models.py
+++ order/models.py
@@ -11,3 +11,6 @@ class Ordering(models.Model):
     total_amount = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+       return self.product_names
