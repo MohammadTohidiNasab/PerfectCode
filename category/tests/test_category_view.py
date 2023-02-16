7e0addbfab4ea32a0c76849cd652a03171cd7d30
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-02-16 12:53:24 -0800

    add some tests

--- category/tests/test_category_view.py
+++ category/tests/test_category_view.py
@@ -1,3 +1,8 @@
+from django.test import Client, TestCase
+import pytest
+from account.models import User
+
+
 def test_category_view(client):
     response = client.get(path = '/')
     assert response.status_code == 200
@@ -9,11 +14,30 @@ def test_account_login(client):
     assert response.status_code == 200
 
 
+class TestPageLogin(TestCase):
+   def setUp(self):
+       self.client = Client()
+   def test_login_page(self):
+       response = self.client.get(path = '/account/login')
+       self.assertEqual(response.status_code, 200)
+       self.assertTemplateUsed(response, 'account/login.html')
+
+
 def test_account_register(client):
     response = client.get(path = '/account/otplogin')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_account_signup(client):
+    user = User.objects.create_user('username','password')
+    client.login(fullname=user.fullname,password='password')
+    response = client.get(path = '/account/otplogin',follow=True)
     assert response.status_code == 200
 
 
 def test_account_logout(client):
     response = client.get(path = '/account/logout')
     assert response.status_code == 302
+#finish account testing
+
