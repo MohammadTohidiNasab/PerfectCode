b2cc45db11bd3ff459d94a089085d017c1118953
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-02-16 03:18:26 -0800

    account tests

--- category/tests/test_category_view.py
+++ category/tests/test_category_view.py
@@ -3,3 +3,17 @@ def test_category_view(client):
     assert response.status_code == 200
 
 
+#account test
+def test_account_login(client):
+    response = client.get(path = '/account/login')
+    assert response.status_code == 200
+
+
+def test_account_register(client):
+    response = client.get(path = '/account/otplogin')
+    assert response.status_code == 200
+
+
+def test_account_logout(client):
+    response = client.get(path = '/account/logout')
+    assert response.status_code == 302
