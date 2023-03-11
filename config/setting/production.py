d067c2d1610cef1e05f3b1b344bf6f040246d493
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-03-11 00:33:02 -0800

    new test

--- config/setting/production.py
+++ config/setting/production.py
@@ -1,4 +1,4 @@
-from . base import *
+from .base import *
 
 
 DEBUG = False
@@ -17,3 +17,9 @@ DATABASES = {
 ALLOWED_HOSTS = [
     'http://127.0.0.1:8000',
 ]
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECRET_KEY = os.environ['SECRET_KEY']
